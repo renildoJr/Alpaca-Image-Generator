af7b775bbf5347764708ba3210fa474d25d629ac
RenildoJr <juninho952020@gmail.com>
2023-09-30 00:20:45 -0300

    JS: Transição de classes em progresso

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 const btnsAcessorize = document.querySelectorAll("#btns-acessorize .btn");
 const btnStylesContainer = document.getElementById("btns-style");
+let btnListRenderedStyle = [];
+let btnStyle = [];
 
 const AlpacaLists = [
     // Hair
@@ -26,27 +28,48 @@ btnsAcessorize.forEach((button, index) => {
     button.addEventListener("click", ()=>{
         classChanging(button, "btn-primary", btnsAcessorize);
         renderButtonList(AlpacaLists[index]);
-    })
-})
+    });
+});
+
+init();
+
+
+
+
+// ideia: criar uma lista com posições estabelecidas, o usuario vai clicar em um
+// botao do acessorize e escolherá um estilo, vai para outro botao do acessorize depois volta
+// para o anterior e a opcao do style tem que continuar a mesma
+
+
 
 function renderButtonList(list = [], selectdButton = 0) { // Na implementação do random, deve-se alterar o selected button para os botões selecionados pelo random conforme clicamos nos acessorizes
     btnStylesContainer.innerHTML = "";
 
-    list.forEach((button, index) => btnStylesContainer.innerHTML+=`
-        <button class="btn btn-round ${index === selectdButton ? "btn-primary" : ""}">${list[index]}</button>
-    `);
+    btnListRenderedStyle = list.map((button, index) => 
+        `<button class="btn btn-round ${index === selectdButton ? "btn-primary" : ""}">${list[index]}</button>`
+    ).join("");
     
-    // return list
+    btnStylesContainer.innerHTML = btnListRenderedStyle;
+    btnStyle = document.querySelectorAll("#btns-style .btn")
+
+    btnStyle.forEach(button => 
+        button.addEventListener("click", ()=> {
+            classChanging(button, "btn-primary", btnStyle);
+        })  
+    )    
+
 }
+
+
 
 function classChanging(currentItem = {}, itemClass = "", itemList = []) {
     itemList.forEach(item => item.classList.remove(itemClass));
     return currentItem.classList.add(itemClass);
 }
 
+
+
 function init() {
     renderButtonList(AlpacaLists[0]);
     btnsAcessorize[0].classList.add("btn-primary");
 }
-
-init();
