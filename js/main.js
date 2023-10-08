8d34de6c795f90d19a2f5fbe90223873226a4afd
RenildoJr <juninho952020@gmail.com>
2023-10-07 21:50:20 -0300

    Criando a funcionalidade de random

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const imageContainer = document.getElementById("image-container");
 const btnsAcessorizeContainer = document.getElementById("btns-acessorize");
 const btnStylesContainer = document.getElementById("btns-style");
+const btnRandom = document.getElementById("btn_random");
 const positions = [4, 0, 0, 0, 0, 0, 3, 0];
 
 const AlpacaLists = [
@@ -16,13 +17,12 @@ const AlpacaLists = [
     {id: 3, category: "mouth",  parts: ["default", "astonished", "eating", "laugh", "tongue"]},
 ];
 
-
-// BUG: TROCANDO A SEQUENCIA DAS IMAGENS APÓS ALTERA-LAS
+// Criar funcionalidades para renderizar todas as imagens quando alteradas e outra para alterar somente a solicitada
 function generateAlpacaImage() {
     imageContainer.innerHTML = "";
     positions.map((position, index) => {
         if(index == 7) {
-            imageContainer.innerHTML+=`<img class="alpaca-component" id="alpaca-nose" src="images/alpaca/nose.png" alt="Alpaca Nose">`;
+            imageContainer.innerHTML+=`<img class="alpaca-component" src="images/alpaca/nose.png" alt="Alpaca Nose">`;
         }
         imageContainer.innerHTML+=`
         <img class="alpaca-component" src="images/alpaca/${AlpacaLists[index].category}/${AlpacaLists[index].parts[position]}.png" alt="Alpaca ${AlpacaLists[index].category}">`;
@@ -31,37 +31,35 @@ function generateAlpacaImage() {
 
 // Init
 function renderAcessorizeButtons() {
-    const AlpacaListSortedById = AlpacaLists.sort((a, b) => a.id - b.id);
-    const buttonList = AlpacaListSortedById.map(button => `<button class="btn btn-round" id="${button.id}">${button.category}</button>`);
+    const buttonList = AlpacaLists.map(button => `<button class="btn btn-round" style="order:${button.id}">${button.category}</button>`);
     
     // Refatorar esse trecho em dentro de uma única função
     btnsAcessorizeContainer.innerHTML = buttonList.join("");
 
     const renderedButtons = Array.from(btnsAcessorizeContainer.querySelectorAll(".btn"));
-    renderedButtons[0].classList.add("btn-primary");
+    renderedButtons[3].classList.add("btn-primary");
     ////
 
     renderedButtons.forEach((button, index) => button.addEventListener("click", ()=>{
         changeButtonsClass("btn-primary", button, renderedButtons);
         // Tornar o código acima reaproveitável, a função abaixo será extra
-        renderStyleButtons(button.id, index);
+        renderStyleButtons(index);
     }));
 }
 
-function renderStyleButtons(CurrentButtonId = 0, currentListIndex = 3) {
-    const buttonList = AlpacaLists[CurrentButtonId].parts.map(button => `<button class="btn btn-round">${button}</button>`);
-    
+function renderStyleButtons(CurrentButtonIndex = 3) {
+    const buttonList = AlpacaLists[CurrentButtonIndex].parts.map(button => `<button class="btn btn-round">${button}</button>`);
     // Refatorar esse trecho em dentro de uma única função
     btnStylesContainer.innerHTML = buttonList.join("");
 
     const renderedButtons = Array.from(btnStylesContainer.querySelectorAll(".btn"));
-    renderedButtons[positions[currentListIndex]].classList.add("btn-primary");
+    renderedButtons[positions[CurrentButtonIndex]].classList.add("btn-primary");
     ////
 
     renderedButtons.forEach((button, index) => button.addEventListener("click", ()=>{
         changeButtonsClass("btn-primary", button, renderedButtons);
         // Tornar o código acima reaproveitável, a função abaixo será extra
-        positions[currentListIndex] = index;
+        positions[CurrentButtonIndex] = index;
         generateAlpacaImage();
     }));
 }
@@ -71,70 +69,15 @@ function changeButtonsClass(ButtonClass = "", currentButton = {}, buttonList = [
     currentButton.classList.add(ButtonClass);
 }
 
+btnRandom.addEventListener("click", randomizePositions);
+
+function randomizePositions() {
+    AlpacaLists.forEach((list, index) =>
+    // continuar daqui 
+        list.parts
+    )
+}
+
 generateAlpacaImage();
 renderAcessorizeButtons();
 renderStyleButtons();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(AlpacaLists[1].parts)
-
-// btnsAcessorize.forEach((button, index) => {
-//     button.addEventListener("click", ()=>{
-//         classChanging(button, "btn-primary", btnsAcessorize);
-//         renderButtonList(AlpacaLists[index].parts[index],positions[index], index);
-//     });
-// });
-
-// // init();
-
-// // ideia: criar uma lista com posições estabelecidas, o usuario vai clicar em um
-// // botao do acessorize e escolherá um estilo, vai para outro botao do acessorize depois volta
-// // para o anterior e a opcao do style tem que continuar a mesma
-
-// function renderButtonList(list = [], selectdButton = 0, triggeredButtonIndex) { // Na implementação do random, deve-se alterar o selected button para os botões selecionados pelo random conforme clicamos nos acessorizes
-//     btnStylesContainer.innerHTML = "";
-
-//     btnListRenderedStyle = list.map((button, index) => 
-//         `<button class="btn btn-round ${index === selectdButton ? "btn-primary" : ""}">${list[index]}</button>`
-//     ).join("");
-    
-//     btnStylesContainer.innerHTML = btnListRenderedStyle;
-//     btnStyle = document.querySelectorAll("#btns-style .btn")
-
-//     btnStyle.forEach((button, index) => 
-//         button.addEventListener("click", ()=> {
-//             classChanging(button, "btn-primary", btnStyle);
-//             positions[triggeredButtonIndex] = index;
-//             console.log(positions)
-//         })  
-//     )
-
-// }
-
-// function classChanging(currentItem = {}, itemClass = "", itemList = []) {
-//     itemList.forEach(item => item.classList.remove(itemClass));
-//     return currentItem.classList.add(itemClass);
-// }
-
-// function init() {
-//     renderButtonList(AlpacaLists[].parts[0]);
-//     btnsAcessorize[0].classList.add("btn-primary");
-// }
