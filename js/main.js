c889781348d85df6625838a6807a1dbb16e38c93
RenildoJr <juninho952020@gmail.com>
2023-10-10 22:37:48 -0300

    Project Finished

--- js/main.js
+++ js/main.js
@@ -11,52 +11,49 @@ const AlpacaLists = [
     {id: 7, category: "backgrounds", parts: ["blue50", "blue60", "blue70", "darkblue30", "darkblue50", "darkblue70", "green50", "green60", "green70", "grey40", "grey70", "grey80", "red50", "red60", "red70", "yellow50", "yellow60", "yellow70"]},
     {id: 4, category: "neck", parts: ["default", "bend-backward", "bend-forward", "thick"]},
     {id: 1, category: "ears", parts: ["default", "tilt-backward", "tilt-forward"]},
-    {id: 0, category: "hair", parts: ["default", "bang", "curls", "elegant", "fancy", "quiff", "short"]},
+    {id: 0, category: "hair", parts: ["default", "bang", "curls", "elegant", "fancy", "short"]},
     {id: 2, category: "eyes", parts: ["default", "angry", "naughty", "panda", "smart", "star"]},
     {id: 5, category: "leg", parts: ["default", "bubble-tea", "cookie", "game-console", "tilt-backward", "tilt-forward"]},
     {id: 6, category: "accessories", parts: ["earings", "flower", "glasses", "headphone"]},
     {id: 3, category: "mouth",  parts: ["default", "astonished", "eating", "laugh", "tongue"]},
 ];
 
-// criar codigo para alterar sequencias 
 
 function generateAlpacaImage() {
     imageContainer.innerHTML = "";
-    positions.map((position, index) => {
-        if(index == 2) {
-            imageContainer.innerHTML+=`<img id="ignore" class="alpaca-component" src="images/alpaca/nose.png" alt="Alpaca Nose">`;
-        }
-        imageContainer.innerHTML+=`<img id="${AlpacaLists[index].id}" class="alpaca-component" src="images/alpaca/${AlpacaLists[index].category}/${AlpacaLists[index].parts[position]}.png" alt="Alpaca ${AlpacaLists[index].category}">`;
-    });
 
+    const generatedImages = positions.map((position, index) => {
+        return `<img id="${AlpacaLists[index].id}" class="alpaca-component" src="images/alpaca/${AlpacaLists[index].category}/${AlpacaLists[index].parts[position]}.png" alt="Alpaca ${AlpacaLists[index].category}">`;
+    });
+   
+
+    generatedImages.splice(2, 0, `<img id="ignore" class="alpaca-component" src="images/alpaca/nose.png" alt="Alpaca Nose">`);
+
+    imageContainer.innerHTML = generatedImages;
+    
     UpdateDownloadLink();
 }
 
 function renderAcessorizeButtons() {
     const buttonList = AlpacaLists.map(button => `<button class="btn btn-round" style="order:${button.id}">${button.category}</button>`);
     
-    // Refatorar esse trecho em dentro de uma única função
     btnsAcessorizeContainer.innerHTML = buttonList.join("");
 
     const renderedButtons = Array.from(btnsAcessorizeContainer.querySelectorAll(".btn"));
     renderedButtons[3].classList.add("btn-primary");
-    ////
 
     renderedButtons.forEach((button, index) => button.addEventListener("click", ()=>{
         changeButtonsClass("btn-primary", button, renderedButtons);
-        // Tornar o código acima reaproveitável, a função abaixo será extra
         renderStyleButtons(index);
     }));
 }
 
 function renderStyleButtons(CurrentButtonIndex = 3) {
     const buttonList = AlpacaLists[CurrentButtonIndex].parts.map(button => `<button class="btn btn-round">${button}</button>`);
-    // Refatorar esse trecho em dentro de uma única função
     btnStylesContainer.innerHTML = buttonList.join("");
 
     const renderedButtons = Array.from(btnStylesContainer.querySelectorAll(".btn"));
     renderedButtons[positions[CurrentButtonIndex]].classList.add("btn-primary");
-    ////
 
     renderedButtons.forEach((button, index) => button.addEventListener("click", ()=>{
         changeButtonsClass("btn-primary", button, renderedButtons);
@@ -76,7 +73,6 @@ function randomizePositions() {
     generateAlpacaImage();
 }
 
-// Html2Canvas
 function UpdateDownloadLink () {
     html2canvas(imageContainer, {width: 350, height: 350}).then(function(canvas) {
         btnDownloadOnClick.href = canvas.toDataURL();
@@ -86,7 +82,6 @@ function UpdateDownloadLink () {
 
 btnRandomOnClick.addEventListener("click", randomizePositions);
 
-// Init
 generateAlpacaImage();
 renderAcessorizeButtons();
 renderStyleButtons();
